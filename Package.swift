720361d8c23301d497e097f099cf16dac706b16d
Titan Jenkins <titan.a@juspay.in>
2025-10-16 16:10:08 +0530

    Updated Package for version 0.1.93

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "JuspaySafeBrowser",
-            url: "https://public.releases.juspay.in/release/ios/juspay-safe-browser/0.1.92/JuspaySafeBrowser.zip",
-            checksum: "89ea99ff1849ee6a7ad04ad27db6cde2c7f755298e0d8b2abaabfdf8db279e4b"
+            url: "https://public.releases.juspay.in/release/ios/juspay-safe-browser/0.1.93/JuspaySafeBrowser.zip",
+            checksum: "6769e80ccf36aa89e6db6f507656f3e9aea232ea5045bd235b4f08d06c9901bc"
         )
     ]
 )
